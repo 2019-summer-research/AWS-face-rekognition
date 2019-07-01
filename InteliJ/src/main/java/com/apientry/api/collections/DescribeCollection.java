30dc626de999abac136dd1579e140a88801947fc
Unknown <snowy6409@comcast.net>
2019-07-01 13:53:37 -0400

    Fixed Manifest Problems

---
+++ InteliJ/src/main/java/com/apientry/api/collections/DescribeCollection.java
@@ -0,0 +1,26 @@
+package com.apientry.api.collections;
+
+import com.apientry.api.ClientFactory;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.model.DescribeCollectionRequest;
+import com.amazonaws.services.rekognition.model.DescribeCollectionResult;
+
+public class DescribeCollection {
+    public void run(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Please provide a collection name.");
+            return;
+        }
+
+        DescribeCollectionRequest request = new DescribeCollectionRequest()
+                .withCollectionId(args[1]);
+
+        AmazonRekognition rekognition = ClientFactory.createClient();
+        DescribeCollectionResult result = rekognition.describeCollection(request);
+
+        System.out.println("ARN: " + result.getCollectionARN()
+                + "\nFace Model Version: " + result.getFaceModelVersion()
+                + "\nFace Count: " + result.getFaceCount()
+                + "\nCreated: " + result.getCreationTimestamp());
+    }
+}
---
+++ InteliJ/src/main/java/com/apientry/api/faces/SearchFacesByImage.java
@@ -0,0 +1,62 @@
+package com.apientry.api.faces;
+
+import com.apientry.api.ClientFactory;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.model.*;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class SearchFacesByImage {
+    public String run(String[] args) {
+
+        if (args.length < 3) {
+            System.err.println("Please provide a collection and images:  ");
+            return "Collection Name and images missing";
+        }
+
+        String collectionId = args[1];
+        String imageArg = args[2];
+
+        Path path = Paths.get(imageArg);
+        ByteBuffer byteBuffer;
+        try {
+            byte[] bytes = Files.readAllBytes(path);
+            byteBuffer = ByteBuffer.wrap(bytes);
+        } catch (IOException e) {
+            System.err.println("Failed to read file '" + imageArg + "': " + e.getMessage());
+            return "Failed to read";
+        }
+
+        SearchFacesByImageRequest request = new SearchFacesByImageRequest()
+                .withCollectionId(collectionId)
+                .withImage(new Image().withBytes(byteBuffer));
+
+        AmazonRekognition rekognition = ClientFactory.createClient();
+        SearchFacesByImageResult result = rekognition.searchFacesByImage(request);
+
+        List<FaceMatch> faceMatches = result.getFaceMatches();
+        for (FaceMatch match : faceMatches) {
+            Float similarity = match.getSimilarity();
+            Face face = match.getFace();
+            System.out.println("MATCH:" +
+                    "\nSimilarity: " + similarity +
+                    "\nFace-ID: " + face.getFaceId() +
+                    "\nImage ID: " + face.getImageId() +
+                    "\nExternal Image ID: " + face.getExternalImageId() +
+                    "\nConfidence: " + face.getConfidence());
+            return face.getExternalImageId();
+        }
+
+        if(result.getFaceMatches().size() < 1)
+        {
+            System.out.println("No Matching Face Found");
+            return "No Face Match Found";
+        }
+        return "No Face";
+    }
+}
